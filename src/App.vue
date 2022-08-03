df65e1fb9567f5f70a65b95af2a3f3eec6280abb
Haroon Sheikh <me.haroonsheikh86@gmail.com>
2022-08-03 16:01:29 -0500

    Timer Game

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Timer Game for demo</h1>
+    <button type="" @click="play()" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+    <Result v-if="showResult" :score="score"/>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Block from './components/Block.vue'
+  import Result from './components/Result.vue'
+  export default{
+    name: 'App',
+    components: { Block, Result },
+    data(){
+      return {
+       isPlaying : false,
+       delay : null,
+       score: null,
+       showResult: false
+      }
+    },
+    methods: {
+      play(){
+        this.delay = 1000 + Math.random() * 5000
+        this.isPlaying = true
+        console.log(this.delay);
+        this.score = 0
+      },
+      endGame(reactionTime){
+        this.score = reactionTime
+        console.log("your total score is",this.score)
+        this.isPlaying = false
+        this.showResult = true
+      }
+      }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    text-align:center;
+    color:#444;
+    margin-top:60px;
+  }
 </style>
+
+
+
